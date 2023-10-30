b00996494d19445c51e40d8b8be95cccf4def8d3
Sreekar Voleti <64281600+sreekar-voleti@users.noreply.github.com>
2023-10-29 20:18:30 -0400

    Update court_params.py
    fixed units for nba and wnba courts

--- court_params.py
+++ court_params.py
@@ -6,14 +6,14 @@ nba_court_parameters = {
     "backboard_distance_from_edge": 4.,
     "backboard_width": 6.,
     # Outer paint
-    "outer_paint_dims": [19., 16],
+    "outer_paint_dims": [18.+5/6, 16-1/3],
     # Inner paint
-    "inner_paint_dims": [19., 12],
+    "inner_paint_dims": [18.+5/6, 12-1/3],
     # Center circle
     "outer_circle_diameter": 6.,
     "inner_circle_diameter": 2.,
     # Three point area
-    "three_point_arc_angle": 68.0,
+    "three_point_arc_angle": 68.13,
     "three_point_arc_diameter": 47.5,
     "three_point_line_length": 14.,
     "three_point_side_width": 3.,
@@ -27,15 +27,15 @@ wnba_court_parameters = {
     "backboard_distance_from_edge": 4.,
     "backboard_width": 6.,
     # Outer paint
-    "outer_paint_dims": [19., 16],
+    "outer_paint_dims": [18.+5/6, 16-1/3],
     # Inner paint
-    "inner_paint_dims": [19., 12],
+    "inner_paint_dims": [18.+5/6, 12-1/3],
     # Center circle
     "outer_circle_diameter": 6.,
     "inner_circle_diameter": 2.,
     # Three point area
-    "three_point_arc_angle": 84.0,
-    "three_point_arc_diameter": 44.29,
+    "three_point_arc_angle": 83.51692630710276,
+    "three_point_arc_diameter": 44.365,
     "three_point_line_length": 7.75,
     "three_point_side_width": 3.,
 }
