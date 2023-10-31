e7ae8f6c7d2238548c4a13a0a2ca0008e6c24b07
Sreekar Voleti <sreekarvoleti3@gmail.com>
2023-10-31 12:32:04 -0400

    Fixed court_params dependency

--- mplbasketball/court.py
+++ mplbasketball/court.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.lines as lines
-import court_params
+from mplbasketball import court_params
 
 
 class LineDataUnits(lines.Line2D):
